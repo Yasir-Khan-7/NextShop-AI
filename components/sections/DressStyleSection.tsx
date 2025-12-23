d5b9d2662e768d9fa7bbd91d18c5e05681ba87c5
yasir-sideline <110191648+Yasir-Khan-7@users.noreply.github.com>
2025-12-23 15:18:50 +0500

    fixed the dress-style card layout

--- components/sections/DressStyleSection.tsx
+++ components/sections/DressStyleSection.tsx
@@ -6,48 +6,64 @@ export default function DressStyleSection() {
   return (
     <section className="py-16">
       <div className="container mx-auto px-4 lg:px-24">
-        <div className="bg-[#F0F0F0] rounded-[40px] p-16">
+        <div className="bg-[#F0F0F0] rounded-[40px] px-16 py-[70px]">
           <SectionTitle>Browse by dress Style</SectionTitle>
           
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
-            {/* Casual */}
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 mt-16">
+            {/* Casual - Small Card */}
             <div className="bg-white rounded-[20px] overflow-hidden relative h-[289px] lg:col-span-1">
               <Image 
                 src="/images/dress-styles/casualcard.svg"
                 alt="Casual Style"
-                fill
-                className="object-cover object-center"
+                width={407}
+                height={289}
+                className="w-full h-full"
               />
+              <h3 className="absolute top-6 left-9 font-satoshi font-bold text-4xl text-black z-10">
+                Casual
+              </h3>
             </div>
             
-            {/* Formal */}
+            {/* Formal - Wide Card */}
             <div className="bg-white rounded-[20px] overflow-hidden relative h-[289px] lg:col-span-2">
               <Image 
                 src="/images/dress-styles/formalcard.svg"
                 alt="Formal Style"
-                fill
-                className="object-cover object-center"
+                width={684}
+                height={289}
+                className="w-full h-full"
               />
+              <h3 className="absolute top-6 left-9 font-satoshi font-bold text-4xl text-black z-10">
+                Formal
+              </h3>
             </div>
             
-            {/* Party */}
+            {/* Party - Wide Card */}
             <div className="bg-white rounded-[20px] overflow-hidden relative h-[289px] lg:col-span-2">
               <Image 
                 src="/images/dress-styles/partycard.svg"
                 alt="Party Style"
-                fill
-                className="object-cover object-center"
+                width={684}
+                height={289}
+                className="w-full h-full"
               />
+              <h3 className="absolute top-6 left-9 font-satoshi font-bold text-4xl text-black z-10">
+                Party
+              </h3>
             </div>
             
-            {/* Gym */}
+            {/* Gym - Small Card */}
             <div className="bg-white rounded-[20px] overflow-hidden relative h-[289px] lg:col-span-1">
               <Image 
                 src="/images/dress-styles/gymcard.svg"
                 alt="Gym Style"
-                fill
-                className="object-cover object-center"
+                width={407}
+                height={289}
+                className="w-full h-full"
               />
+              <h3 className="absolute top-6 left-9 font-satoshi font-bold text-4xl text-black z-10">
+                Gym
+              </h3>
             </div>
           </div>
         </div>
